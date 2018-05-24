1929be406d103613f2d6fe7e3eef13cfbf77969d
Patrick Huber <patrick.huber1@gmail.com>
2018-05-24 09:55:52 -0500

    test writing new camera document to firestore

--- connect-wifi.js
+++ connect-wifi.js
@@ -3,6 +3,13 @@ var bleno = require('bleno');
 var wifi = require('pi-wifi');
 var jsonfile = require('jsonfile');
 
+const Firestore = require('@google-cloud/firestore');
+
+const firestore = new Firestore({
+  projectId: 'oven-cam',
+  keyFilename: './oven-cam-keystore.json',
+});
+
 
 var BlenoCharacteristic = bleno.Characteristic;
 
@@ -44,23 +51,108 @@ EchoCharacteristicConnectWifi.prototype.onWriteRequest = function(data, offset, 
     this._updateValueCallback(this._value);
   }
 
+  // Mock function to run without configuring wifi
   var connect_wifi = 'success';
+  // var connect_wifi = 'fail';
+
+  // Temp variables for testing
+  var ssid = wifiCredentials[0];
+  var status.ip = '10.0.0.59';
+
 
   if (connect_wifi === 'success') {
+    console.log('Connected to the network ' + ssid + '! IP: ' + status.ip);
+    
+    // Write to firestore
+    var cam_id = '';
+
+    let collectionRef = firestore.collection('cameras');
+
+    // Add new camera document and return document id
+    collectionRef.add({
+      battery_level: 100, // Todo: need to get from pi-power
+      charging: false, // Todo: need to get from pi-power
+      local_ip: status.ip + ':3000', // Don't forget to add 3000 port on the express server
+      name: 'Oven cam',
+      status: 'online'
+    }).then(documentReference => {  
+      // Todo: need to send back cam_id to client
+      cam_id = documentReference.id;
+      console.log(`New document id: ${cam_id}`);
+    });
+
     // Update status json file so we don't run setup again
     var file = './status.json'
     var obj = {isSetup: true}
-     
     jsonfile.writeFile(file, obj, function (err) {
       // console.error(err);
-    })
-    console.log('Successful connection!');
+    });
+
     callback(this.RESULT_SUCCESS);
     this._value = data;
+
     // Start express server
     var express_server = require('./bin/www');
   }
 
+  var network = {
+    ssid: wifiCredentials[0],
+    password: wifiCredentials[1]
+  };
+  /*
+  wifi.connectTo(network, function(err) {
+    if (!err) { //Network created correctly
+      setTimeout(function () {
+        wifi.check(ssid, function (err, status) {
+          if (!err && status.connected) {
+            console.log('Connected to the network ' + ssid + '! IP: ' + status.ip);
+            
+            // Write to firestore
+            var cam_id = '';
+
+            let collectionRef = firestore.collection('cameras');
+
+            // Add new camera document and return document id
+            collectionRef.add({
+              battery_level: 100, // Todo: need to get from pi-power
+              charging: false, // Todo: need to get from pi-power
+              local_ip: status.ip + ':3000', // Don't forget to add 3000 port on the express server
+              name: 'Oven cam',
+              status: 'online'
+            }).then(documentReference => {  
+              // Todo: need to send back cam_id to client
+              cam_id = documentReference.id;
+              console.log(`New document id: ${cam_id}`);
+            });
+
+            // Update status json file so we don't run setup again
+            var file = './status.json'
+            var obj = {isSetup: true}
+            jsonfile.writeFile(file, obj, function (err) {
+              // console.error(err);
+            });
+
+            callback(this.RESULT_SUCCESS);
+            this._value = data;
+
+            // Start express server
+            var express_server = require('./bin/www');
+          } else {
+            console.log('Unable to connect to the network ' + ssid + '!');
+            console.error(err.message);
+            callback(false);
+          }
+        });
+      }, 2000);
+    } else {
+      console.log('Unable to create the network ' + ssid + '.');
+      console.error(err.message);
+      callback(false);
+    }
+  });
+  */
+
+  // Legacy wifi connect. Remove after checking wifi.connectTo function above
   // wifi.connect(wifiCredentials[0], wifiCredentials[1], function(err) {
   //   if (err) {
   //     console.error(err.message);
