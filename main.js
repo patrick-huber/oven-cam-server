c96983508ba522a0971ba25bd5e04a73e9885a52
Patrick Huber <patrick.huber1@gmail.com>
2018-06-08 11:40:28 -0500

    always run ble at startup - may want to revise in the future if power drain is an issue

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ var jsonfile = require('jsonfile');
 
 var status = '/home/pi/oven-cam-server/status.json';
 jsonfile.readFile(status, function(err, obj) {
+  var setup = require('./ble');
   if(!err && obj.isSetup) {
     // Setup complete. Run express
     var express = require('./bin/www');
-  } else {
-    var setup = require('./ble');
   }
 });
